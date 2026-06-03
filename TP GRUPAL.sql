3d7e5d3d802363a5bb7298280f723678618d86ca
Alejo0421 <alebustamante@uade.edu.ar>
2026-06-03 11:17:00 -0300

    Agregación de Inserts y Drops tables
    Inserts mediante create procedure de todas las tablas junto a los drops tables if exists

--- TP GRUPAL.sql
+++ TP GRUPAL.sql
@@ -5,15 +5,16 @@ go
 use tpgrupal
 go
 
-DROP TABLE entrega
-DROP TABLE encuesta
-DROP TABLE documentacion
-DROP TABLE detalle_proyecto
-DROP TABLE proyecto
-DROP TABLE Interaccion
-DROP TABLE producto
-DROP TABLE asesor
-DROP TABLE cliente
+DROP TABLE IF EXISTS entrega;
+DROP TABLE IF EXISTS encuesta;
+DROP TABLE IF EXISTS documentacion;
+DROP TABLE IF EXISTS detalle_proyecto;
+DROP TABLE IF EXISTS entrega;
+DROP TABLE IF EXISTS proyecto;
+DROP TABLE IF EXISTS Interaccion;
+DROP TABLE IF EXISTS producto;
+DROP TABLE IF EXISTS asesor;
+DROP TABLE IF EXISTS cliente;
 
 CREATE TABLE cliente(
 	Id_cliente INT IDENTITY(1,1) PRIMARY KEY,
@@ -115,14 +116,64 @@ CREATE TABLE detalle_proyecto(
 );
 
 --------INSERTADO DE DATOS----
-INSERT INTO cliente
-(Nombre, Apellido, Email, Telefono, Direccion, Medio_de_contacto)
-VALUES
-('Martina','Gomez','martina.gomez@gmail.com',1123456789,'Av Santa Fe 2450','Instagram'),
-('Ricardo','Perez','ricardo.perez@gmail.com',1134567890,'Cabildo 1550','Showroom'),
-('Lucia','Fernandez','lucia.fernandez@gmail.com',1145678901,'Libertador 3300','Referido'),
-('Andres','Lopez','andres.lopez@gmail.com',1156789012,'Maipu 890','Facebook'),
-('Sofia','Martinez','sofia.martinez@gmail.com',1167890123,'Parana 1200','Mail');
+DROP PROCEDURE IF EXISTS AgregarCliente;
+GO
+
+CREATE PROCEDURE AgregarCliente
+	@Nombre VARCHAR(50),
+	@Apellido VARCHAR(50),
+	@Email VARCHAR(50),
+	@Telefono INT,
+	@Direccion VARCHAR(50),
+	@Medio_de_contacto VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO	cliente(NombreC,ApellidoC,EmailC,TelefonoC,DireccionC,Medio_de_contacto)
+	VALUES(@Nombre,@Apellido,@Email,@Telefono,@Direccion,@Medio_de_contacto)
+
+END;
+
+EXEC AgregarCliente
+'Juan',
+'Perez',
+'juan@gmail.com',
+11223344,
+'Av Rivadavia 123',
+'WhatsApp';
+
+EXEC AgregarCliente
+'Maria',
+'Lopez',
+'marialopez@gmail.com',
+1133445566,
+'San Martin 456',
+'WhatsApp';
+
+EXEC AgregarCliente
+'Carlos',
+'Gomez',
+'carlosgomez@hotmail.com',
+1144556677,
+'Belgrano 789',
+'Telefono';
+
+EXEC AgregarCliente
+'Lucia',
+'Fernandez',
+'luciaf@gmail.com',
+1155667788,
+'Cabildo 321',
+'WhatsApp';
+
+EXEC AgregarCliente
+'Martin',
+'Diaz',
+'martind@hotmail.com',
+1166778899,
+'Mitre 654',
+'Email';
+
+SELECT*FROM cliente
 
 -- =========================
 -- ASESORES
@@ -162,124 +213,226 @@ GO
 -- =========================
 -- PROYECTOS
 -- =========================
-INSERT INTO proyecto VALUES
-(1,11,'2026-01-10','Finalizado',1800000,10,'2026-02-15'),
-(1,12,'2026-01-18','En proceso',2400000,5,'2026-03-20'),
-(3,13,'2026-02-01','Pendiente',980000,0,'2026-04-01'),
-(4,14,'2026-02-12','Finalizado',1470000,15,'2026-03-05'),
-(5,15,'2026-03-01','En proceso',1950000,8,'2026-04-15'),
-(6,16,'2026-03-05','Finalizado',2200000,12,'2026-04-10'),
-(7,17,'2026-03-11','Pendiente',850000,0,'2026-05-01'),
-(8,18,'2026-03-20','En proceso',3100000,7,'2026-05-25'),
-(9,19,'2026-04-02','Finalizado',2750000,10,'2026-05-15'),
-(10,20,'2026-04-08','En proceso',1650000,5,'2026-06-01');
+DROP PROCEDURE IF EXISTS AgregarProyecto;
+GO
+
+CREATE PROCEDURE AgregarProyecto
+	@Id_cliente INT,
+	@Id_asesor INT,
+	@Fecha_de_inicio DATE,
+	@Estado VARCHAR(50),
+	@Monto DECIMAL(12,2),
+	@Descuento DECIMAL(12,2),
+	@Fecha_de_entrega DATE
+AS
+BEGIN
+	INSERT INTO	proyecto(Id_cliente,Id_asesor,Fecha_de_inicio,Estado,Monto,Descuento,Fecha_de_entrega)
+	VALUES(@Id_cliente,@Id_asesor,@Fecha_de_inicio,@Estado,@Monto,@Descuento,@Fecha_de_entrega)
+END;
+
+EXEC AgregarProyecto 1,11,'2026-01-10','Finalizado',1800000,10,'2026-02-15';
+
+EXEC AgregarProyecto 2,12,'2026-01-18','En proceso',2400000,5,'2026-03-20';
+
+EXEC AgregarProyecto 3,13,'2026-02-01','Pendiente',980000,0,'2026-04-01';
+
+EXEC AgregarProyecto 4,14,'2026-02-12','Finalizado',1470000,15,'2026-03-05';
+
+EXEC AgregarProyecto 5,15,'2026-03-01','En proceso',1950000,8,'2026-04-15';
+
+
+SELECT*FROM proyecto
 
 
 -- =========================
 -- INTERACCIONES
 -- =========================
-INSERT INTO Interaccion VALUES
-(1,11,'2026-01-05','WhatsApp','Consulta inicial','Interesado'),
-(2,12,'2026-01-08','Email','Envio presupuesto','En negociacion'),
-(3,13,'2026-01-15','Telefono','Consulta cocina','Interesado'),
-(4,14,'2026-01-28','WhatsApp','Solicita descuento','Pendiente'),
-(5,15,'2026-02-05','Presencial','Firma contrato','Cerrada'),
-(6,16,'2026-02-20','Email','Consulta tecnica','Interesado'),
-(7,17,'2026-03-02','Telefono','Confirma compra','Cerrada'),
-(8,18,'2026-03-10','WhatsApp','Solicita visita','Interesado'),
-(9,19,'2026-03-18','Email','Aprueba dise�o','En negociacion'),
-(10,20,'2026-03-29','Presencial','Entrega se�al','Cerrada');
+DROP PROCEDURE IF EXISTS AgregarInteraccion;
+GO
 
+CREATE PROCEDURE AgregarInteraccion
+	@Id_cliente INT,
+	@Id_asesor INT,
+	@Fecha_Interaccion DATE,
+	@Medio VARCHAR(50),
+	@Comentarios VARCHAR(50),
+	@Estado_de_venta VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO	Interaccion(Id_cliente,Id_asesor,Fecha_Interaccion,Medio,Comentarios,Estado_de_venta)
+	VALUES(@Id_cliente,@Id_asesor,@Fecha_Interaccion,@Medio,@Comentarios,@Estado_de_venta)
+
+END;
+
+EXEC AgregarInteraccion 1,11,'2026-01-05','WhatsApp','Consulta inicial','Interesado';
+
+EXEC AgregarInteraccion 2,12,'2026-01-08','Email','Envio presupuesto','En negociacion';
+
+EXEC AgregarInteraccion 3,13,'2026-01-15','Telefono','Consulta cocina','Interesado';
+
+EXEC AgregarInteraccion 4,14,'2026-01-28','WhatsApp','Solicita descuento','Pendiente';
+
+EXEC AgregarInteraccion 5,15,'2026-02-05','Presencial','Firma contrato','Cerrada';
+
+SELECT*FROM Interaccion
 
 -- =========================
 -- ENTREGAS
 -- =========================
-INSERT INTO entrega
-(Id_proyecto, fecha_programada, fecha_real, direccion, estado)
-VALUES
-(1,'2025-07-15',NULL,'Av Santa Fe 2450','Pendiente'),
-(2,'2025-08-01',NULL,'Cabildo 1550','Pendiente'),
-(3,'2025-08-10',NULL,'Libertador 3300','Pendiente'),
-(4,'2025-07-30','2025-07-30','Maipu 890','Completada'),
-(5,'2025-08-20','2025-08-21','Parana 1200','Completada');
+DROP PROCEDURE IF EXISTS AgregarEntrega;
+GO
+
+CREATE PROCEDURE AgregarEntrega
+	@Id_proyecto INT,
+	@fecha_programada DATE,
+	@fecha_real DATE,
+	@direccion_entrega VARCHAR(50),
+	@estado VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO	entrega(Id_proyecto,fecha_programada,fecha_real,direccion_entrega,estado)
+	VALUES(@Id_proyecto,@fecha_programada,@fecha_real,@direccion_entrega,@estado)
+
+END;
+
+EXEC AgregarEntrega 10,'2026-05-12','2026-05-15','Riobamba 510','Pendiente';
+
+EXEC AgregarEntrega 20,'2026-05-02','2026-05-04','Rivadavia 3100','Pendiente';
+
+EXEC AgregarEntrega 30,'2026-04-16','2026-04-19','Callao 300','Completada';
+
+EXEC AgregarEntrega 40,'2026-06-24','2026-06-29','Corrientes 1200','Cancelada';
+
+EXEC AgregarEntrega 50,'2026-05-06','2026-05-09','Belgrano 201','Completada';
+
+SELECT*FROM entrega
 
 
 -- =========================
 -- ENCUESTAS
 -- =========================
-INSERT INTO encuesta VALUES
-(10,'2026-02-20',9,'Muy conforme','SI'),
-(20,'2026-03-25',7,'Buena atencion','SI'),
-(30,'2026-04-05',6,'Demora en entrega','NO'),
-(40,'2026-03-10',10,'Excelente servicio','SI'),
-(50,'2026-04-20',8,'Buen resultado','SI'),
-(60,'2026-04-15',9,'Muy recomendable','SI'),
-(70,'2026-05-05',5,'Falta comunicacion','NO'),
-(80,'2026-05-30',8,'Muy buen dise�o','SI'),
-(90,'2026-05-18',10,'Todo perfecto','SI'),
-(100,'2026-06-05',7,'Buen trabajo','SI');
+DROP PROCEDURE IF EXISTS AgregarEncuesta;
+GO
+
+CREATE PROCEDURE AgregarEncuesta
+	@Id_proyecto INT,
+	@Fecha_encuesta DATE,
+	@Puntuacion INT,
+	@Comentarios VARCHAR(50),
+	@Volveria_a_comprar VARCHAR(2)
+AS
+BEGIN
+	INSERT INTO	encuesta(Id_proyecto,Fecha_encuesta,Puntuacion,Comentarios,Volveria_a_comprar)
+	VALUES(@Id_proyecto,@Fecha_encuesta,@Puntuacion,@Comentarios,@Volveria_a_comprar)
+
+END;
+
+EXEC AgregarEncuesta 10,'2026-02-20',9,'Muy conforme','SI';
+
+EXEC AgregarEncuesta 20,'2026-03-25',7,'Buena atencion','SI';
+
+EXEC AgregarEncuesta 30,'2026-04-05',6,'Demora en entrega','NO';
+
+EXEC AgregarEncuesta 40,'2026-03-10',10,'Excelente servicio','SI';
+
+EXEC AgregarEncuesta 50,'2026-04-20',8,'Buen resultado','SI';
+
+SELECT*FROM encuesta
 
 
 -- =========================
 -- DOCUMENTACION
 -- =========================
-INSERT INTO documentacion VALUES
-(10,'Contrato','contrato1.pdf','/docs/contratos','2026-01-09'),
-(20,'Plano','plano2.pdf','/docs/planos','2026-01-18'),
-(30,'Render','render3.jpg','/docs/renders','2026-02-01'),
-(40,'Factura','factura4.pdf','/docs/facturas','2026-02-15'),
-(50,'Presupuesto','presupuesto5.pdf','/docs/presupuestos','2026-03-01'),
-(60,'Contrato','contrato6.pdf','/docs/contratos','2026-03-06'),
-(70,'Plano','plano7.pdf','/docs/planos','2026-03-12'),
-(80,'Render','render8.jpg','/docs/renders','2026-03-22'),
-(90,'Factura','factura9.pdf','/docs/facturas','2026-04-05'),
-(100,'Presupuesto','presupuesto10.pdf','/docs/presupuestos','2026-04-10');
+DROP PROCEDURE IF EXISTS AgregarDocumentacion;
+GO
 
-select * from producto;
+CREATE PROCEDURE AgregarDocumentacion
+	@Id_proyecto INT,
+	@Tipo VARCHAR(50),
+	@Nombre_archivo VARCHAR(50),
+	@Ruta VARCHAR(50),
+	@Fecha_de_carga DATE
+AS
+BEGIN
+	INSERT INTO	documentacion(Id_proyecto,Tipo,Nombre_archivo,Ruta,Fecha_de_carga)
+	VALUES(@Id_proyecto,@Tipo,@Nombre_archivo,@Ruta,@Fecha_de_carga)
+
+END;
+
+EXEC AgregarDocumentacion 10,'Contrato','contrato1.pdf','/docs/contratos','2026-01-09';
+
+EXEC AgregarDocumentacion 20,'Plano','plano2.pdf','/docs/planos','2026-01-18';
+
+EXEC AgregarDocumentacion 30,'Render','render3.jpg','/docs/renders','2026-02-01';
+
+EXEC AgregarDocumentacion 40,'Factura','factura4.pdf','/docs/facturas','2026-02-15';
+
+EXEC AgregarDocumentacion 50,'Presupuesto','presupuesto5.pdf','/docs/presupuestos','2026-03-01';
+
+SELECT*FROM documentacion
 -- =========================
 -- PRODUCTO
 -- =========================
 
-insert into PRODUCTO
-(NOMBRE,CATEGORIA,MARCA,DESCRIPCION,PRECIO_UNITARIO)
-values
-('Bajo mesada Oslo','COCINA','Johnson','Mueble bajo mesada',450000),
-('Alacena Premium','COCINA','Johnson','Alacena blanca laqueada',320000),
-('Isla Central Nova','COCINA','Johnson','Isla moderna de cocina',780000),
-('Placard Verona','PLACARD','Reno','Placard corredizo 3 puertas',650000),
-('Vestidor Milano','PLACARD','Johnson','Vestidor modular completo',980000),
-('Placard Compact','PLACARD','Reno','Placard compacto juvenil',390000),
-('Horno Empotrable','ELECTRODOMESTICOS','Samsung','Horno electrico digital',850000),
-('Anafe Vitroceramico','ELECTRODOMESTICOS','Whirlpool','Anafe vitroceramico 4 hornallas',620000),
-('Campana Extractora','ELECTRODOMESTICOS','TST','Campana de acero inoxidable',410000),
-('Lavavajillas Smart','ELECTRODOMESTICOS','Bosch','Lavavajillas automatico',1100000);
+DROP PROCEDURE IF EXISTS AgregarProducto;
+GO
+
+CREATE PROCEDURE AgregarProducto
+	@NombreP VARCHAR(50),
+	@Categoria VARCHAR(50),
+	@Marca VARCHAR(50),
+	@Descripcion VARCHAR(50),
+	@Precio_unitario decimal (12,2)
+AS
+BEGIN
+	INSERT INTO producto(NombreP,Categoria,Marca,Descripcion,Precio_unitario)
+	VALUES(@NombreP,@Categoria,@Marca,@Descripcion,@Precio_unitario)
+
+END;
+
+EXEC AgregarProducto 'Bajo mesada Oslo','COCINA','Johnson','Mueble bajo mesada',450000;
+
+EXEC AgregarProducto 'Alacena Premium','COCINA','Johnson','Alacena blanca laqueada',320000;
+
+EXEC AgregarProducto 'Isla Central Nova','COCINA','Johnson','Isla moderna de cocina',780000;
+
+EXEC AgregarProducto 'Placard Verona','PLACARD','Reno','Placard corredizo 3 puertas',650000;
+
+EXEC AgregarProducto 'Vestidor Milano','PLACARD','Johnson','Vestidor modular completo',980000;
+
+SELECT*FROM producto
 
 
 -- =========================
 -- DETALLE_PROYECTO
 -- =========================
-INSERT INTO detalle_proyecto VALUES
-(10,1001,1,'Bajo mesada principal'),
-(20,1002,1,'Alacena superior'),
-(30,1003,1,'Isla moderna'),
-(40,1004,1,'Anafe instalado'),
-(50,1005,1,'Vestidor completo'),
-(60,1006,2,'Placares corredizos'),
-(70,1007,1,'Horno digital'),
-(80,1008,1,'Campana acero inoxidable'),
-(90,1009,2,'Placard juvenil'),
-(100,1010,1,'Lavavajillas premium');
+DROP PROCEDURE IF EXISTS AgregarDetalle_producto
+GO
 
+CREATE PROCEDURE AgregarDetalle_producto
+	@Id_proyecto INT,
+	@Id_producto INT,
+	@Cantidad INT,
+	@Observaciones_tecnicas VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO detalle_proyecto(Id_proyecto,Id_producto,Cantidad,Observaciones_tecnicas)
+	VALUES(@Id_proyecto,@Id_producto,@Cantidad,@Observaciones_tecnicas)
 
-SELECT*FROM cliente
-SELECT*FROM asesor
-SELECT*FROM proyecto
-SELECT*FROM Interaccion
-SELECT*FROM entrega
-SELECT*FROM encuesta
-SELECT*FROM documentacion
-SELECT*FROM producto
+END;
+
+EXEC AgregarDetalle_producto 10,1001,1,'Bajo mesada principal';
+
+EXEC AgregarDetalle_producto 20,1002,1,'Alacena superior';
+
+EXEC AgregarDetalle_producto 30,1003,1,'Isla moderna';
+
+EXEC AgregarDetalle_producto 40,1004,1,'Anafe instalado';
+
+EXEC AgregarDetalle_producto 50,1005,1,'Vestidor completo';
+
 SELECT*FROM detalle_proyecto
+
 
 ----FUNCION_CALCULAR----
 
